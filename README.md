fa95848b8a64a6ef8f5ee01d1ab6350dbdb0b2ed
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-02-29 23:30:35 +0700

    fixed docstrings

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ from dynamic_sitemap import FlaskSitemap
 app = Flask(__name__)
 sitemap = FlaskSitemap(app, 'https://mysite.com')
 sitemap.config.IGNORED.extend(['/edit', '/upload'])
-sitemap.config.FOLDER = ('..', 'static',)
+sitemap.config.TEMPLATE_FOLDER = ['app', 'templates']
 sitemap.add_rule('/app', Post, lastmod='created')
 sitemap.add_rule('/app/tag', Tag, priority=0.4)
 app.add_url_rule('/sitemap.xml', endpoint='sitemap', view_func=sitemap.view)
@@ -27,7 +27,7 @@ sm_logger = logging.getLogger('sitemap')
 sm_logger.setLevel(30)
 
 class Config:
-    FOLDER = ('public',)
+    TEMPLATE_FOLDER = ['app', 'templates']
     IGNORED = ['/admin', '/back-office', '/other-pages']
     ALTER_PRIORITY = 0.1
     LOGGER = sm_logger
--- dynamic_sitemap/main.py
+++ dynamic_sitemap/main.py
@@ -13,7 +13,7 @@ Basic example:
     app = Flask(__name__)
     sitemap = FlaskSitemap(app, 'https://mysite.com')
     sitemap.config.IGNORED.extend(['/edit', '/upload'])
-    sitemap.config.FOLDER = ('..', 'static',)
+    sitemap.config.TEMPLATE_FOLDER = ['app', 'templates']
     sitemap.add_rule('/app', Post, lastmod='created')
     sitemap.add_rule('/app/tag', Tag, priority=0.4)
     app.add_url_rule('/sitemap.xml', endpoint='sitemap', view_func=sitemap.view)
@@ -24,7 +24,7 @@ IGNORED has a priority over add_rule. Also you can set configurations from your 
     sm_logger.setLevel(30)
 
     class Config:
-        FOLDER = ('public',)
+        TEMPLATE_FOLDER = ['app', 'templates']
         IGNORED = ['/admin', '/back-office', '/other-pages']
         ALTER_PRIORITY = 0.1
         LOGGER = sm_logger
