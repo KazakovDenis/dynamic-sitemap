45e98d9e90cb3aff5a4243b0fa35c1aeee7c0744
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-03-02 09:33:52 +0700

    test: copy template exception

--- dynamic_sitemap/tests/unit/test_negative.py
+++ dynamic_sitemap/tests/unit/test_negative.py
@@ -5,3 +5,12 @@ from ..conf import *
 def test_priority_01(flask_map, priority):
     with pytest.raises((AssertionError, TypeError)):
         flask_map.add_rule('/app', Model, priority=priority)
+
+
+def test_default_copy_exception(default_map, request):
+    def test_default_copy_exception_teardown():
+        default_map.config.DEBUG = True
+    request.addfinalizer(test_default_copy_exception_teardown)
+    default_map.config.DEBUG = False
+    with pytest.raises(FileExistsError):
+        default_map._copy_template(template_folder)
